eb5f8aa69fc00b12955281b4473497b85538c5f4
JAragon <jaragon@spcdatapro.com>
2021-12-15 11:59:28 -0600

    Backend para tiempo de entrega. 15/12/2021 11:59.

--- application/call_center/controllers/Tiempo_entrega.php
+++ application/call_center/controllers/Tiempo_entrega.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Tipo_direccion extends CI_Controller
+class Tiempo_entrega extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         $this->load->model([
-            'Tipo_direccion_model'
+            'Tiempo_entrega_model'
         ]);
 
         $this->load->helper(['jwt', 'authorization']);
@@ -21,23 +21,23 @@ class Tipo_direccion extends CI_Controller
 
     public function buscar()
     {
-        $datos = $this->Tipo_direccion_model->buscar($_GET);
-        $datos = ordenar_array_objetos($datos, 'descripcion');
+        $datos = $this->Tiempo_entrega_model->buscar($_GET);
+        $datos = ordenar_array_objetos($datos, 'orden', 1);
         $this->output->set_output(json_encode($datos));
     }
 
     public function guardar($id = '')
     {
-        $tipodir = new Tipo_direccion_model($id);
+        $tiempoEntrega = new Tiempo_entrega_model($id);
         $req = json_decode(file_get_contents('php://input'), true);
         $datos = ['exito' => false];
         if ($this->input->method() == 'post') {
-            $datos['exito'] = $tipodir->guardar($req);
+            $datos['exito'] = $tiempoEntrega->guardar($req);
             if ($datos['exito']) {
                 $datos['mensaje'] = "Datos actualizados con éxito.";
-                $datos['tipo_direccion'] = $tipodir;
+                $datos['tiempo_entrega'] = $tiempoEntrega;
             } else {
-                $datos['mensaje'] = $tipodir->getMensaje();
+                $datos['mensaje'] = $tiempoEntrega->getMensaje();
             }
         } else {
             $datos['mensaje'] = "Parámetros inválidos.";
