576fd0572a054ab78976ad233efd04f023aa8c05
JAragon <jaragon@spcdatapro.com>
2021-12-29 16:11:03 -0600

    Fix de punto 254 de QA. 29/12/2021 16:11.

--- web/src/app/restaurante/components/turno-tipo/lista-turno/lista-turno.component.ts
+++ web/src/app/restaurante/components/turno-tipo/lista-turno/lista-turno.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
-import { PageEvent } from '@angular/material/paginator';
+import { Component, OnInit, Output, EventEmitter, ViewChild, OnDestroy } from '@angular/core';
+import { PageEvent, MatPaginator } from '@angular/material/paginator';
 import { PaginarArray, MultiFiltro } from '../../../../shared/global';
 
 import { TipoTurno } from '../../../interfaces/tipo-turno';
 import { TipoTurnoService } from '../../../services/tipo-turno.service';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-turno-tipo',
   templateUrl: './lista-turno.component.html',
   styleUrls: ['./lista-turno.component.css']
 })
-export class ListaTurnoTipoComponent implements OnInit {
+export class ListaTurnoTipoComponent implements OnInit, OnDestroy {
 
   public lstTurnos: TipoTurno[];
   public lstTurnosPaged: TipoTurno[];
   @Output() getTurnoEv = new EventEmitter();
+  @ViewChild('paginador') paginador: MatPaginator;
 
   public length = 0;
   public pageSize = 5;
@@ -23,13 +26,19 @@ export class ListaTurnoTipoComponent implements OnInit {
   public pageEvent: PageEvent;
   public txtFiltro = '';
 
+  private endSubs = new Subscription();
+
   constructor(private turnoSrvc: TipoTurnoService) { }
 
   ngOnInit() {
     this.loadTurnos();
   }
 
-  applyFilter() {
+  ngOnDestroy() {
+    this.endSubs.unsubscribe();
+  }
+
+  applyFilter(cambioPagina = false) {
     if (this.txtFiltro.length > 0) {
       const tmpList = MultiFiltro(this.lstTurnos, this.txtFiltro);
       this.length = tmpList.length;
@@ -37,6 +46,9 @@ export class ListaTurnoTipoComponent implements OnInit {
     } else {
       this.length = this.lstTurnos.length;
       this.lstTurnosPaged = PaginarArray(this.lstTurnos, this.pageSize, this.pageIndex + 1);
+    }
+    if (!cambioPagina) {
+      this.paginador.firstPage();
     }
   }
 
@@ -58,7 +70,7 @@ export class ListaTurnoTipoComponent implements OnInit {
   pageChange = (e: PageEvent) => {
     this.pageSize = e.pageSize;
     this.pageIndex = e.pageIndex;
-    this.applyFilter();
+    this.applyFilter(true);
   }
 
 }
