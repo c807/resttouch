549e18f85dae277a94ef8d5628870442a2b037d2
JAragon <jaragon@spcdatapro.com>
2022-05-05 09:14:22 -0600

    Actualización de variable de versión. 05/05/2022 09:14.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -5,7 +5,7 @@ const urlBase = `${PROTOCOLO}//${ANFITRION}/${LOCALHOST.indexOf(ANFITRION) < 0 ?
 import * as moment from 'moment';
 
 export const GLOBAL = {
-  rtVersion: '2022.04.26.11.22.39',
+  rtVersion: '2022.05.05.08.06.04',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
