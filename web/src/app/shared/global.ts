d247372b4d4fa3da2802358b1c64d050b5007a0c
JAragon <jaragon@spcdatapro.com>
2022-12-07 16:01:03 -0600

    Fix para no poder trasladar mesas a sí mismas. 07/12/2022 16:01.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -5,7 +5,7 @@ const urlBase = `${PROTOCOLO}//${ANFITRION}/${LOCALHOST.indexOf(ANFITRION) < 0 ?
 import * as moment from 'moment';
 
 export const GLOBAL = {
-  rtVersion: '2022.12.07.14.32.33',
+  rtVersion: '2022.12.07.15.58.04',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
