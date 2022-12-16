3b986ee8a1bdf631c950765ba681a2d0a47dad91
JAragon <jaragon@spcdatapro.com>
2022-12-16 08:08:59 -0600

    Bugfix al grabar nuevo cliente en call center. 16/12/2022 08:08.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -5,7 +5,7 @@ const urlBase = `${PROTOCOLO}//${ANFITRION}/${LOCALHOST.indexOf(ANFITRION) < 0 ?
 import * as moment from 'moment';
 
 export const GLOBAL = {
-  rtVersion: '2022.12.15.07.24.58',
+  rtVersion: '2022.12.16.07.51.21',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
