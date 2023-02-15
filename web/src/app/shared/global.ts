cdd428e96b4fb7b49392cdd63fb8330caeb4bf04
Javier <jaragon@spcdatapro.com>
2023-02-15 07:12:14 -0600

    Actualización de variable de versión. 15/02/2023 07:12.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -7,7 +7,7 @@ import { Municipio } from '@admin-interfaces/municipio';
 import { Cliente } from '@admin-interfaces/cliente';
 
 export const GLOBAL = {
-  rtVersion: '2023.02.14.15.47.23',
+  rtVersion: '2023.02.15.08.00.00',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
