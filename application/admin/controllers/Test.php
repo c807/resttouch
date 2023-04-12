5b12f1bf6e55cad9077bdbb692c20ecb55993b1b
Javier <jaragon@spcdatapro.com>
2023-04-11 20:51:55 -0600

    Se agregó paquete para conectarse con OpenAI. Endpoint de prueba para conectarse a OpenAI. 11/04/2023 20:51.

--- application/admin/controllers/Test.php
+++ application/admin/controllers/Test.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+use Orhanerday\OpenAi\OpenAi;
+
 class Test extends CI_Controller
 {
     public function __construct()
@@ -22,5 +24,42 @@ class Test extends CI_Controller
     public function osinfo()
     {
         $this->output->set_output(json_encode(['OS' => PHP_OS]));
+    }
+
+    public function openai()
+    {
+        $open_ai_key = getenv('OPENAI_API_KEY');
+        $open_ai = new OpenAi($open_ai_key);
+        $open_ai->setORG('org-vLm4cxV8YfAVn8YRpxDeEk8C');
+
+        $chat = $open_ai->chat([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                [
+                    "role" => "system",
+                    "content" => "You are a helpful assistant."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Who won the world series in 2020?"
+                ],
+                [
+                    "role" => "assistant",
+                    "content" => "The Los Angeles Dodgers won the World Series in 2020."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Where was it played?"
+                ],
+            ],
+            'temperature' => 1.0,
+            'max_tokens' => 4000,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
+        ]);
+
+        $d = json_decode($chat);
+
+        $this->output->set_output(json_encode($d));
     }
 }
