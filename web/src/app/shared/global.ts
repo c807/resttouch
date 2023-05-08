859ddce2e26982156deeb4089a8b43cd8d3d03e3
Javier <jaragon@spcdatapro.com>
2023-05-08 17:47:19 -0600

    Resolución de punto de QA #75. (2a. version). Mejora para ver alias de sede en reportes de WMS. 08/05/2023 17:47.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -8,7 +8,7 @@ import { Municipio } from '@admin-interfaces/municipio';
 import { Cliente } from '@admin-interfaces/cliente';
 
 export const GLOBAL = {
-  rtVersion: '2023.05.08.08.13.03',
+  rtVersion: '2023.05.08.17.47.10',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
