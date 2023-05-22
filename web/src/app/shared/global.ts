1366a4e1201bdf6c95e58718b4f1fda401146acd
Javier <jaragon@spcdatapro.com>
2023-05-22 09:17:45 -0600

    Fix a punto #167 de QA. 22/05/2023 09:17.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -8,7 +8,7 @@ import { Municipio } from '@admin-interfaces/municipio';
 import { Cliente } from '@admin-interfaces/cliente';
 
 export const GLOBAL = {
-  rtVersion: '2023.05.21.20.59.29',
+  rtVersion: '2023.05.22.09.15.36',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
