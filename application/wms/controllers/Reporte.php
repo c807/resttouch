5601692405f588e3a753054aacd7f550ea36987c
Javier <jaragon@spcdatapro.com>
2023-11-02 09:23:36 -0600

    Fix en descarga de excel de ingresos, egresos y requisisiones individuales por uso de &. 02/11/2023 09:23.

--- application/wms/controllers/Reporte.php
+++ application/wms/controllers/Reporte.php
@@ -947,6 +947,7 @@ class Reporte extends CI_Controller
 
 		if ((int)$excel === 1) {
 			$reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+			$vista = str_replace('&', '&amp;', $vista);
 			$xlsx = $reader->loadFromString($vista);
 			$xlsx->setActiveSheetIndex(0);
 			$hoja = $xlsx->getActiveSheet();
@@ -1013,6 +1014,7 @@ class Reporte extends CI_Controller
 
 		if ((int)$excel === 1) {
 			$reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+			$vista = str_replace('&', '&amp;', $vista);
 			$xlsx = $reader->loadFromString($vista);
 			$xlsx->setActiveSheetIndex(0);
 			$hoja = $xlsx->getActiveSheet();
