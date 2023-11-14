fb19f5e988c223cf1673c9a200d2d9c2bb40a063
Javier <jaragon@spcdatapro.com>
2023-11-14 08:33:14 -0600

    Se quitó que el certificador sea requerido en el mantenimiento de las sedes. Actualización de variable de version. 14/11/2023 08:33.

--- web/src/app/shared/global.ts
+++ web/src/app/shared/global.ts
@@ -8,7 +8,7 @@ import { Municipio } from '@admin-interfaces/municipio';
 import { Cliente } from '@admin-interfaces/cliente';
 
 export const GLOBAL = {
-  rtVersion: '2023.11.13.08.17.15',
+  rtVersion: '2023.11.14.08.08.39',
   dbDateFormat: 'YYYY-MM-DD',
   dbDateTimeFormat: 'YYYY-MM-DD HH:mm:ss',
   dbDateTimeFormatMilli: 'YYYY-MM-DD HH:mm:ss.SSS',
