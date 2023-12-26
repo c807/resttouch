972f20be4cd7c362a092b68e0f26d04d0dd0e23d
Javier <jaragon@spcdatapro.com>
2023-12-26 16:59:09 -0600

    Fix en oc que pasaba los que tienen cantidad 0 y daba error en cálculo de costo. 26/12/2023 16:59.

--- application/wms/models/Compra_model.php
+++ application/wms/models/Compra_model.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Compra_model extends General_Model {
+class Compra_model extends General_Model
+{
 
 	public $orden_compra;
 	public $sede;
@@ -17,7 +18,7 @@ class Compra_model extends General_Model {
 		parent::__construct();
 		$this->setTabla('orden_compra');
 
-		if(!empty($id)) {
+		if (!empty($id)) {
 			$this->cargar($id);
 		}
 	}
@@ -25,18 +26,18 @@ class Compra_model extends General_Model {
 	public function getProveedor()
 	{
 		return $this->db
-					->where('proveedor', $this->proveedor)
-					->get('proveedor')
-					->row();
+			->where('proveedor', $this->proveedor)
+			->get('proveedor')
+			->row();
 	}
 
-	public function setDetalle(Array $args, $id = '')
+	public function setDetalle(array $args, $id = '')
 	{
 		$det = new CDetalle_Model($id);
 		$args['orden_compra'] = $this->orden_compra;
 		$result = $det->guardar($args);
 
-		if($result) {
+		if ($result) {
 			return $det;
 		}
 
@@ -49,15 +50,15 @@ class Compra_model extends General_Model {
 	{
 		$args['orden_compra'] = $this->orden_compra;
 		$det = $this->CDetalle_model->buscar($args);
-		$datos = [] ;
-		if(is_array($det)) {
+		$datos = [];
+		if (is_array($det)) {
 			foreach ($det as $row) {
 				$detalle = new CDetalle_Model($row->orden_compra_detalle);
 				$row->articulo = $detalle->getArticulo();
 				$row->presentacion = $detalle->getPresentacion();
 				$datos[] = $row;
 			}
-		} else if($det) {
+		} else if ($det) {
 			$detalle = new CDetalle_Model($det->orden_compra_detalle);
 			$det->articulo = $detalle->getArticulo();
 			$det->presentacion = $detalle->getPresentacion();
@@ -67,7 +68,7 @@ class Compra_model extends General_Model {
 		return $datos;
 	}
 
-	public function generarIngreso($args =[])
+	public function generarIngreso($args = [])
 	{
 		$ing = new Ingreso_model();
 		$datos = [
@@ -81,85 +82,86 @@ class Compra_model extends General_Model {
 		];
 
 		$this->load->model([
-			'BodegaArticuloCosto_model', 
-			'Bodega_model', 
-			'Presentacion_model', 
-			'Articulo_model', 
-			'Sede_model', 
+			'BodegaArticuloCosto_model',
+			'Bodega_model',
+			'Presentacion_model',
+			'Articulo_model',
+			'Sede_model',
 			'Receta_model'
 		]);
 		$sede = new Sede_model($this->data->sede);
 		$emp = $sede->getEmpresa();
 		$iva = 1 + $emp->porcentaje_iva;
 
-		if($ing->guardar($datos)) {
+		if ($ing->guardar($datos)) {
 			$bac = new BodegaArticuloCosto_model();
 			$bod = new Bodega_model($ing->bodega);
-			foreach ($this->getDetalle() as $row) {
+			$detOcs = $this->getDetalle();
+			foreach ($detOcs as $row) {
+				if ((float)$row->cantidad > (float)0) {
+					$costo = $row->monto / $iva;
+					$row->articulo = $row->articulo->articulo;
+					$row->presentacion = $row->presentacion->presentacion;
+					$row->precio_unitario = $costo;
+					$row->precio_total = $costo * $row->cantidad;
+					$row->precio_costo_iva = $row->precio_total * $emp->porcentaje_iva;
+					$pres = new Presentacion_model($row->presentacion);
 
-				$costo = $row->monto / $iva;
-				$row->articulo = $row->articulo->articulo;
-				$row->presentacion = $row->presentacion->presentacion;
-				$row->precio_unitario = $costo;
-				$row->precio_total = $costo * $row->cantidad;
-				$row->precio_costo_iva = $row->precio_total * $emp->porcentaje_iva;
-				$pres = new Presentacion_model($row->presentacion);
-
-				$det = $ing->setDetalle((array) $row);
-				if($det) {
-					$art = new Articulo_model($row->articulo);
-					$art->actualizarExistencia([
-						"bodega" => $ing->bodega,
-						"sede" => $bod->sede
-					]);
-
-					$bcosto = $this->BodegaArticuloCosto_model->buscar([
-						'bodega' => $ing->bodega, 
-						'articulo' => $art->getPK(), 
-						'_uno' => true
-					]);
-
-					$costo = $art->getCosto(["bodega" => $ing->bodega]);
-
-					if ($bcosto) {
-						$bac->cargar($bcosto->bodega_articulo_costo);
-						/*Ultima compra*/
-						$costo_uc = $art->getCosto([
-							"bodega" => $ing->bodega, 
-							"metodo_costeo" => 1
+					$det = $ing->setDetalle((array) $row);
+					if ($det) {
+						$art = new Articulo_model($row->articulo);
+						$art->actualizarExistencia([
+							"bodega" => $ing->bodega,
+							"sede" => $bod->sede
 						]);
-						$bac->costo_ultima_compra = $costo_uc;
 
-						/*Costo promedio*/
-						$costo = $bcosto->costo_promedio * $art->existencias + $row->precio_total;
-						$existencia = $art->existencias + $row->cantidad * $pres->cantidad;
-						if ($existencia != 0) {
-							$costo = $costo / $existencia;
-						} 
+						$bcosto = $this->BodegaArticuloCosto_model->buscar([
+							'bodega' => $ing->bodega,
+							'articulo' => $art->getPK(),
+							'_uno' => true
+						]);
 
-						$bac->costo_promedio = $costo;
-						
+						$costo = $art->getCosto(["bodega" => $ing->bodega]);
+
+						if ($bcosto) {
+							$bac->cargar($bcosto->bodega_articulo_costo);
+							/*Ultima compra*/
+							$costo_uc = $art->getCosto([
+								"bodega" => $ing->bodega,
+								"metodo_costeo" => 1
+							]);
+							$bac->costo_ultima_compra = $costo_uc;
+
+							/*Costo promedio*/
+							$costo = $bcosto->costo_promedio * $art->existencias + $row->precio_total;
+							$existencia = $art->existencias + $row->cantidad * $pres->cantidad;
+							if ($existencia != 0) {
+								$costo = $costo / $existencia;
+							}
+
+							$bac->costo_promedio = $costo;
+						} else {
+							$bac->bodega = $ing->bodega;
+							$bac->articulo = $art->getPK();
+							$bac->costo_ultima_compra = $costo ?? 0;
+							$bac->costo_promedio = $costo ?? 0;
+						}
+
+						$art->guardar(["costo" => $costo]);
+						$bac->guardar();
+
+						if ((int)$ing->ajuste === 0) {
+							$this->Ingreso_model->actualiza_ultima_compra($ing, $det, $row->monto);
+						}
 					} else {
-						$bac->bodega = $ing->bodega;
-						$bac->articulo = $art->getPK();
-						$bac->costo_ultima_compra = $costo;
-						$bac->costo_promedio = $costo;
+						$this->mensaje = $ing->getMensaje();
 					}
-
-					$art->guardar(["costo" => $costo]);
-					$bac->guardar();
-
-					if((int)$ing->ajuste === 0) {						
-						$this->Ingreso_model->actualiza_ultima_compra($ing, $det, $row->monto);
-					}
-				} else {
-					$this->mensaje = $ing->getMensaje();
 				}
 			}
 			$this->db
-				 ->set('ingreso', $ing->ingreso)
-				 ->set('orden_compra', $this->orden_compra)
-				 ->insert('ingreso_has_orden_compra');
+				->set('ingreso', $ing->ingreso)
+				->set('orden_compra', $this->orden_compra)
+				->insert('ingreso_has_orden_compra');
 			return $ing;
 		} else {
 			$this->mensaje = $ing->getMensaje();
@@ -167,7 +169,6 @@ class Compra_model extends General_Model {
 
 		return false;
 	}
-
 }
 
 /* End of file Compra_model.php */
