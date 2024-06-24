025318550930b2d0d6e5be0ff250c9f45fc865a3
Javier <jaragon@spcdatapro.com>
2024-06-24 17:36:34 -0600

    Halar montos extras en reporte de ventas por articulos comandados. 24/06/2024 17:36.

--- application/facturacion/models/Rpt_articulo_comandado_model.php
+++ application/facturacion/models/Rpt_articulo_comandado_model.php
@@ -1,9 +1,34 @@
 <?php
 class Rpt_articulo_comandado_model extends General_model
 {
+
+    private function calculaPrecioExtra($detalle_comanda = null, $precioExtra = 0) {
+        if (!$detalle_comanda) {
+            return $precioExtra;
+        }
+
+        $res = $this->db->select('SUM(total) AS total, GROUP_CONCAT(DISTINCT detalle_comanda SEPARATOR ",") detalle_comanda')
+            ->where("detalle_comanda_id IN({$detalle_comanda})")
+            ->get('detalle_comanda')
+            ->row();
+        
+        if ($res) {
+            $precioExtra += (float)$res->total;
+            $detalle_comanda = $res->detalle_comanda;
+            if ($detalle_comanda) {
+                $precioExtra += $this->calculaPrecioExtra($detalle_comanda);
+            }            
+        }
+
+        return $precioExtra;
+    }
+
     public function get_comandas_con_detalle($args = [])
     {
-        $this->db->select('GROUP_CONCAT(DISTINCT b.comanda SEPARATOR ", ") AS comanda, TRIM(CONCAT(TRIM(s.nombre), IFNULL(CONCAT(" (", s.alias, ")"), ""))) AS sede, d.descripcion AS articulo, SUM(a.cantidad) AS cantidad, SUM(a.total) AS total, f.descripcion AS turno_tipo');
+        $campos = 'GROUP_CONCAT(DISTINCT b.comanda SEPARATOR ", ") AS comanda, TRIM(CONCAT(TRIM(s.nombre), IFNULL(CONCAT(" (", s.alias, ")"), ""))) AS sede, d.descripcion AS articulo, SUM(a.cantidad) AS cantidad, ';
+        $campos.= 'SUM(a.total) AS total, f.descripcion AS turno_tipo, ';
+        $campos.= 'GROUP_CONCAT(DISTINCT a.detalle_comanda SEPARATOR ",") AS detalle_comanda';
+        $this->db->select($campos);
         $this->db->from('detalle_comanda a');
         $this->db->join('comanda b', 'b.comanda = a.comanda');
         $this->db->join('turno c', 'c.turno = b.turno');
@@ -23,22 +48,25 @@ class Rpt_articulo_comandado_model extends General_model
             }
         }
 
-        // if (isset($args['turno_tipo']) && !is_null($args['turno_tipo']) && (int)$args['turno_tipo'] > 0) {
-        //     $this->db->where('c.turno_tipo', $args['turno_tipo']);
-        // }
-
         if (isset($args['fdel'])) {
             $this->db->where('DATE(a.fecha) >=', $args['fdel']);
         }
 
         if (isset($args['fal'])) {
             $this->db->where('DATE(a.fecha) <=', $args['fal']);
-        }
+        }        
         
-        // $this->db->group_by('s.nombre, s.alias, a.articulo, d.descripcion, f.descripcion');
         $this->db->group_by('s.sede, d.articulo');
         $this->db->order_by('2, d.descripcion');
         $result = $this->db->get()->result();
+
+        $cntArticulosVendidos = count($result);
+        for ($i = 0; $i < $cntArticulosVendidos; $i++) {
+            $articuloVendido = $result[$i];
+            $precioExtra = $this->calculaPrecioExtra($articuloVendido->detalle_comanda);
+            $articuloVendido->total = (float)$articuloVendido->total + $precioExtra;
+        }
+
         return $result;
     }
 }
